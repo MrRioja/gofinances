a273e21dc14994b66b166f2850c7b77b4e880f17
Luiz Rioja <lulyrioja@gmail.com>
2021-08-28 16:39:15 -0300

    :alien: Creating and Styling the Dashboard Screen

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Title, Container } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard screen</Title>
+    </Container>
+  );
+}
