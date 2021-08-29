4707717a9b02b6300e5fdb19b954ec5ab7e68565
Luiz Rioja <lulyrioja@gmail.com>
2021-08-28 23:37:21 -0300

    :alien: Adding user components to the header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,39 @@
 import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
 
-import { Title, Container } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard screen</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/55336456?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Hello,</UserGreeting>
+              <UserName>Luiz Rioja</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCard />
     </Container>
   );
 }
