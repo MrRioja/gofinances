c43d9f8abe691ebd5ecb915da99d737bfe4d5151
Luiz Rioja <lulyrioja@gmail.com>
2021-08-29 00:26:57 -0300

    :alien: Styling highlightCards and correcting dashboard details

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
@@ -13,7 +14,7 @@ export const Header = styled.View`
   height: ${RFPercentage(42)}px;
   background-color: ${({ theme }) => theme.colors.primary};
 
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
   justify-content: center;
 `;
@@ -21,6 +22,8 @@ export const Header = styled.View`
 export const UserWrapper = styled.View`
   width: 100%;
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -29,11 +32,12 @@ export const UserWrapper = styled.View`
 export const UserInfo = styled.View`
   flex-direction: row;
   align-items: center;
+  justify-content: center;
 `;
 
 export const Photo = styled.Image`
-  width: ${RFValue(48)}px;
-  height: ${RFValue(48)}px;
+  width: ${RFValue(50)}px;
+  height: ${RFValue(50)}px;
 
   border-radius: 10px;
 `;
@@ -44,17 +48,27 @@ export const User = styled.View`
 
 export const UserGreeting = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
-  font-size: ${RFValue(18)}px;
+  font-size: ${RFValue(17)}px;
   font-family: ${({ theme }) => theme.fonts.regular};
 `;
 
 export const UserName = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
-  font-size: ${RFValue(18)}px;
+  font-size: ${RFValue(17)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
 `;
 
 export const Icon = styled(Feather)`
   color: ${({ theme }) => theme.colors.secondary};
   font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 },
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(23)}px;
 `;
