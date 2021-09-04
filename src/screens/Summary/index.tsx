344036f73a28c0c95d3bf4acdc3fea7f1d8407e9
Luiz Rioja <lulyrioja@gmail.com>
2021-09-04 20:58:07 -0300

    :alien: Implementing and styling the Summary screen

--- src/screens/Summary/index.tsx
+++ src/screens/Summary/index.tsx
@@ -1,10 +1,88 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { HistoryCard } from "../../components/HistoryCard";
+
+import { Container, Header, Title, Content } from "./styles";
+
+import { categories } from "../../utils/categories";
+
+interface TransactionData {
+  type: "positive" | "negative";
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+}
+
+interface CategoryData {
+  key: string;
+  name: string;
+  total: string;
+  color: string;
+}
 
 export function Summary() {
+  const [totalByCategory, setTotalByCategory] = useState<CategoryData[]>([]);
+
+  async function loadData() {
+    const dataKey = "@goFinance:transactions";
+    const response = await AsyncStorage.getItem(dataKey);
+    const responseFormatted = response ? JSON.parse(response) : [];
+
+    const expensive = responseFormatted.filter(
+      (expensive: TransactionData) => expensive.type === "negative"
+    );
+
+    const totalByCategory: CategoryData[] = [];
+
+    categories.forEach((category) => {
+      let categorySum = 0;
+
+      expensive.forEach((expensive: TransactionData) => {
+        if (expensive.category === category.key) {
+          categorySum += Number(expensive.amount);
+        }
+      });
+
+      if (categorySum > 0) {
+        const total = categorySum.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        });
+
+        totalByCategory.push({
+          key: category.key,
+          name: category.name,
+          color: category.color,
+          total,
+        });
+      }
+    });
+
+    setTotalByCategory(totalByCategory);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
-    <View>
-      <Text>Summary page</Text>
-    </View>
+    <Container>
+      <Header>
+        <Title>Summary by category</Title>
+      </Header>
+
+      <Content>
+        {totalByCategory.map((item) => (
+          <HistoryCard
+            key={item.key}
+            title={item.name}
+            amount={item.total}
+            color={item.color}
+          />
+        ))}
+      </Content>
+    </Container>
   );
 }
