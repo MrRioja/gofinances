246aeb694690995e99b7a3197e558ef16504e1bf
Luiz Rioja <lulyrioja@gmail.com>
2021-09-05 21:05:14 -0300

    :alien: Creating authentication hook

---
+++ src/Hooks/auth.tsx
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import { createContext, ReactNode } from "react";
+import { string } from "yup/lib/locale";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+interface IAuthContextData {
+  user: User;
+}
+
+const AuthContext = createContext({} as IAuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const user = {
+    id: "1234",
+    name: "Luiz Rioja",
+    email: "luiz@gmail.com",
+  };
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
