c5b12f91e3411fc698b1897128646ac387b40b2c
Luiz Rioja <lulyrioja@gmail.com>
2021-10-17 23:53:32 -0300

    :alien: Creating the input component tests

--- src/components/Form/Input/input.spec.tsx
+++ src/components/Form/Input/input.spec.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { render } from "@testing-library/react-native";
 
 import { Input } from ".";
+import { ThemeProvider } from "styled-components/native";
+import theme from "../../../global/styles/theme";
+
+const Providers: React.FC = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
 
 describe("Input Component", () => {
   it("must have specific border color when active", () => {
@@ -12,12 +18,15 @@ describe("Input Component", () => {
         keyboardType="email-address"
         autoCorrect={false}
         active={true}
-      />
+      />,
+      { wrapper: Providers }
     );
 
     const inputComponent = getByTestId("input-email");
 
-    expect(inputComponent.props.style[0].borderColor).toEqual("#E83F5B");
+    expect(inputComponent.props.style[0].borderColor).toEqual(
+      theme.colors.attention
+    );
     expect(inputComponent.props.style[0].borderWidth).toEqual(3);
   });
 });
