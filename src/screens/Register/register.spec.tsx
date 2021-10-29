163ec793461604a0a82c07d9c057b4e7201cb2eb
Luiz Rioja <lulyrioja@gmail.com>
2021-10-28 23:52:04 -0300

    :alien: Starting Register Screen Test Implementation

---
+++ src/screens/Register/register.spec.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+import { Register } from ".";
+
+import { ThemeProvider } from "styled-components/native";
+import theme from "../../global/styles/theme";
+
+const Providers: React.FC = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+describe("Register screen", () => {
+  it("should be open category modal when user click on button", () => {
+    const { getByTestId } = render(<Register />, { wrapper: Providers });
+
+    const categoryModal = getByTestId("modal-category");
+    expect(categoryModal.props.visible).toBeFalsy();
+  });
+});
